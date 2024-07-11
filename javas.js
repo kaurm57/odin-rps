8bb6165c5319bfee7fed8135a67c2f0b5b09ba7d
Manarpan Kaur <kaurm57@mcmaster.ca>
2024-07-11 14:22:12 +0530

    added ui

--- javas.js
+++ javas.js
@@ -12,53 +12,63 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
-
-    let humanChoice = prompt("Choose rock, paper or scissors")
-
-    return humanChoice
+function getHumanChoice(id){
+    return id
 }
 
 function playRound(humanChoice, computerChoice){
-    if (computerChoice == "rock"){
-        if (humanChoice == "rock"){
-            return "tie"
-        }
-        else if (humanChoice == "paper"){
-            return "win"
-        }
-        else{
-            return "lose"
-        }
+    if (humanChoice === computerChoice){
+        return "tie"
+    }
+    else if (computerChoice == "rock"){
+        return (humanChoice == "paper")? "win": "lose";
+
+
     }
     else if (computerChoice == "paper"){
-        if (humanChoice == "rock"){
-            return "lose"
-        }
-        else if (humanChoice == "paper"){
-            return "tie"
-        }
-        else{
-            return "win"
-        }
+        return (humanChoice == "rock")? "lose": "win";
     }
     else if (computerChoice == "scissors"){
-        if (humanChoice == "rock"){
-            return "win"
-        }
-        else if (humanChoice == "paper"){
-            return "lose"
-        }
-        else{
-            return "tie"
-        }
+        return (humanChoice == "rock")? "win": "lose";
     }
 }
+
 let i = 0;
 let humanScore = 0;
 let computerScore = 0;
 
-while (i < 5){
+const btn = document.querySelectorAll("button");
+
+btn.forEach((button) => {
+    button.addEventListener("click", function(){
+        if (humanScore < 5 && computerScore < 5){
+            let humanChoice = getHumanChoice(button.id)
+        let computerChoice = getComputerChoice()
+        let result = playRound(humanChoice, computerChoice)
+        if (result == "win"){
+            humanScore++
+        }
+        else if (result == "lose"){
+            computerScore++
+        }
+        alert("It's a " + result)
+        console.log(`Human: ${humanScore} Computer: ${computerScore}`)
+        }
+        else{
+            let winner = (humanScore > computerScore)? "Human": "Computer";
+            alert(winner + " wins");
+        }
+    })
+})
+
+if (humanScore >=5){
+    alert("Human wins");
+}
+else if (computerScore >= 5){
+    alert("Computer wins");
+};
+
+/**while (i < 5){
     let humanChoice = getHumanChoice()
     let computerChoice = getComputerChoice()
     let result = playRound(humanChoice, computerChoice)
@@ -70,4 +80,4 @@ while (i < 5){
     }
     console.log(`Human: ${humanScore} Computer: ${computerScore}`)
     i++
-}
+} **/
